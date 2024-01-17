fa1800f2cd3378163e349ebef8a4ef1efb8db52f
kaspersoreide <kasper.soreide@gmail.com>
2024-01-17 15:46:20 +0100

    made 3d line trees

--- shaders/compute/simpleInterpret.glsl
+++ shaders/compute/simpleInterpret.glsl
@@ -16,12 +16,36 @@ uniform layout (location = 0) uint stringLength;
 uniform layout (location = 1) float segmentLength;
 uniform layout (location = 2) float turnAngle;
 
-mat4 rotate(float a) {
+// found at https://www.neilmendoza.com/glsl-rotation-about-an-arbitrary-axis/
+// rotates counterclockwise hopefully
+mat4 rotationMatrix(vec3 axis, float angle) {
+    vec3 a = normalize(axis);
+
+    float s = -sin(angle);
+    float c = cos(angle);
+
+    float oc = 1.0 - c;
+
+    float sx = s * a.x;
+    float sy = s * a.y;
+    float sz = s * a.z;
+
+    float ocx = oc * a.x;
+    float ocy = oc * a.y;
+    float ocz = oc * a.z;
+
+    float ocxx = ocx * a.x;
+    float ocxy = ocx * a.y;
+    float ocxz = ocx * a.z;
+    float ocyy = ocy * a.y;
+    float ocyz = ocy * a.z;
+    float oczz = ocz * a.z;
+
     return mat4(
-        cos(a), -sin(a), 0.0, 0.0,
-        sin(a), cos(a), 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0, 1.0
+        vec4(ocxx + c, ocxy - sz, ocxz + sy, 0.0),
+        vec4(ocxy + sz, ocyy + c, ocyz - sx, 0.0),
+        vec4(ocxz - sy, ocyz + sx, oczz + c, 0.0),
+        vec4(0.0, 0.0, 0.0, 1.0)
     );
 }
 
@@ -34,35 +58,62 @@ mat4 translate(vec3 v) {
     );
 }
 
-
+struct State {
+    mat4 transform;
+    float segmentLength;
+};
 
 
 void main() {
     int top = -1;
-    vec4 stateStack[128]; //x and y is pos, z is angle, w is step
-    vec4 state;   
-    state.w = segmentLength;
-    //float angle = 0.1;
+    State stateStack[32];
+    State currentState;
+    //set initial state pointing upwards (y-axis)
+    currentState.transform = mat4(
+        vec4(0.0, 1.0, 0.0, 0.0),
+        vec4(1.0, 0.0, 0.0, 0.0),
+        vec4(0.0, 0.0, 1.0, 0.0),
+        vec4(0.0, 0.0, 0.0, 1.0)
+    );
+    currentState.segmentLength = segmentLength;
+    
     for (uint i = 0; i < stringLength; i++) {
         switch (string[i]) {
-            case 43:    //+ 
-                state.z += turnAngle;
+            case 43:    // + turn left
+                currentState.transform = currentState.transform * rotationMatrix(vec3(0.0, 0.0, 1.0), turnAngle);
                 break;
-            case 45:    //-
-                state.z -= turnAngle;
+            case 45:    // - turn right
+                currentState.transform = currentState.transform * rotationMatrix(vec3(0.0, 0.0, 1.0), -turnAngle);
                 break;
-            case 91:    //[
-                stateStack[++top] = state;
-                state.w *= 0.8;
+            case 38:    // & pitch down
+                currentState.transform = currentState.transform * rotationMatrix(vec3(0.0, 1.0, 0.0), turnAngle);
                 break;
-            case 93:    //]
-                state = stateStack[top--];
+            case 94:    // ^ pitch up
+                currentState.transform = currentState.transform * rotationMatrix(vec3(0.0, 1.0, 0.0), -turnAngle);
+                break;
+            case 92:    // \ roll left
+                currentState.transform = currentState.transform * rotationMatrix(vec3(1.0, 0.0, 0.0), turnAngle);
+                break;
+            case 47:    // / roll right
+                currentState.transform = currentState.transform * rotationMatrix(vec3(1.0, 0.0, 0.0), -turnAngle);
+                break;
+            case 91:    // [ push state
+                stateStack[++top] = currentState;
+                currentState.segmentLength *= 0.7;
+                break;
+            case 93:    // ] pop state
+                currentState = stateStack[top--];
                 break; 
-            default:    //go forward
-                vertices[2 * i] = vec4(state.xy, 0.0, 1.0);
-                vec2 forward = vec2(cos(state.z), sin(state.z));
-                state.xy += state.w * forward;
-                vertices[2 * i + 1] = vec4(state.xy, 0.0, 1.0);
+            default:    // go forward
+                //vertices[2 * i] = vec4(state.xy, 0.0, 1.0);
+                //vec2 forward = vec2(cos(state.z), sin(state.z));
+                //state.xy += state.w * forward;
+                //vertices[2 * i + 1] = vec4(state.xy, 0.0, 1.0);
+                vertices[2 * i] = vec4(currentState.transform[3]);
+                vec3 forward = (currentState.transform * vec4(1.0, 0.0, 0.0, 0.0)).xyz;
+                currentState.transform = translate(currentState.segmentLength * forward) * currentState.transform;
+                vertices[2 * i + 1] = vec4(currentState.transform[3]);
+                currentState.segmentLength *= 0.9;
                 break;
         };
         //vertices[2 * i] = vec4(0.001 * i, 0.0, 0.0, 1.0);
