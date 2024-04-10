3e3b6d86f2e921a8ef2392b7c71b1cfe5cfb8d26
kaspersoreide <kasper.soreide@gmail.com>
2024-04-10 14:00:46 +0200

    added procedural bark texture

--- shaders/tree/raycaster.glsl
+++ shaders/tree/raycaster.glsl
@@ -97,54 +97,102 @@ vec3 quadraticSpline(vec3 a, vec3 b, vec3 c, float t) {
     vec3 bc = mix(b, c, t);
     return mix(ab, bc, t);
 }
-/*
-vec3 closestSplinePoint(uint idx, vec3 point) {
-    //cubic spline
-    float curve = 0.01;
-    float distFactor = 0.5;
-    vec3 p3 = (Model * tree[idx].pos).xyz;
-    vec3 p0 = (Model * tree[tree[idx].parent].pos).xyz;
-    vec3 dir = normalize(p3 - p0);
-    vec3 parentDir = getParentDir(idx);
-    float d = distance(p0, p3);
-    vec3 p2 = p3 - d * distFactor * dir;// + curve * randomVec3(p3);
-    vec3 p1 = p0 + d * distFactor * parentDir;// - curve * randomVec3(p0);
-    float t = closestParameter(p0, p3, point);
-    return cubicSpline(p0, p1, p2, p3, t);
+
+float sum2(vec2 v) { return dot(v, vec2(1)); }
+float sat(float x) { return clamp(x, 0.0, 1.0); }
+
+
+///////////////////////////////////////////////////////////////////////////////
+//procedural wood texture shader code retrieved from https://www.shadertoy.com/view/mdy3R1
+
+float h31(vec3 p3) {
+	p3 = fract(p3 * .1031);
+	p3 += dot(p3, p3.yzx + 333.3456);
+	return fract(sum2(p3.xy) * p3.z);
 }
 
-float splineDist(uint idx, vec3 point) {
-    vec3 splinePoint = closestSplinePoint(idx, point);
-    return distance(point, splinePoint) 
-        - mix(tree[idx].width, tree[tree[idx].parent].width, 0.5);
+float h21(vec2 p) { return h31(p.xyx); }
+
+float n31(vec3 p) {
+	const vec3 s = vec3(7, 157, 113);
+
+	// Thanks Shane - https://www.shadertoy.com/view/lstGRB
+	vec3 ip = floor(p);
+	p = fract(p);
+	p = p * p * (3. - 2. * p);
+	vec4 h = vec4(0, s.yz, sum2(s.yz)) + dot(ip, s);
+	h = mix(fract(sin(h) * 43758.545), fract(sin(h + s.x) * 43758.545), p.x);
+	h.xy = mix(h.xz, h.yw, p.y);
+	return mix(h.x, h.y, p.z);
 }
 
-vec3 splineNormal(uint idx, vec3 point) {
-    vec3 splinePoint = closestSplinePoint(idx, point);
-    return normalize(point - splinePoint);
+// roughness: (0.0, 1.0], default: 0.5
+// Returns unsigned noise [0.0, 1.0]
+float fbm(vec3 p, int octaves, float roughness) {
+	float sum = 0.,
+	      amp = 1.,
+	      tot = 0.;
+	roughness = sat(roughness);
+	for (int i = 0; i < octaves; i++) {
+		sum += amp * n31(p);
+		tot += amp;
+		amp *= roughness;
+		p *= 2.;
+	}
+	return sum / tot;
 }
 
-vec3 cylinderNormal(uint idx, vec3 point) {
-    //https://gdbooks.gitbooks.io/3dcollisions/content/Chapter1/closest_point_on_line.html
-    vec3 a = (Model * tree[idx].pos).xyz;
-    vec3 b = (Model * tree[tree[idx].parent].pos).xyz;
-    float t = closestParameter(a, b, point);
-    vec3 closestPoint = a + (b - a) * t;
-    return normalize(point - closestPoint);
+vec3 randomPos(float seed) {
+	vec4 s = vec4(seed, 0, 1, 2);
+	return vec3(h21(s.xy), h21(s.xz), h21(s.xw)) * 1e2 + 1e2;
 }
-*/
-/*
-vec2 replaceMins(vec2 mins, float value) {
-    if (value < mins[0]) {
-        mins[1] = mins[0];
-        mins[0] = value;
-    }
-    else if (value < mins[1]) {
-        mins[1] = value;
-    }
-    return mins;
+
+// Returns unsigned noise [0.0, 1.0]
+float fbmDistorted(vec3 p) {
+	p += (vec3(n31(p + randomPos(0.)), n31(p + randomPos(1.)), n31(p + randomPos(2.))) * 2. - 1.) * 1.12;
+	return fbm(p, 8, .5);
 }
-*/
+
+// vec3: detail(/octaves), dimension(/inverse contrast), lacunarity
+// Returns signed noise.
+float musgraveFbm(vec3 p, float octaves, float dimension, float lacunarity) {
+	float sum = 0.,
+	      amp = 1.,
+	      m = pow(lacunarity, -dimension);
+	for (float i = 0.; i < octaves; i++) {
+		float n = n31(p) * 2. - 1.;
+		sum += n * amp;
+		amp *= m;
+		p *= lacunarity;
+	}
+	return sum;
+}
+
+// Wave noise along X axis.
+vec3 waveFbmX(vec3 p) {
+	float n = p.x * 20.;
+	n += .4 * fbm(p * 3., 3, 3.);
+	return vec3(sin(n) * .5 + .5, p.yz);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Math
+float remap01(float f, float in1, float in2) { return sat((f - in1) / (in2 - in1)); }
+
+///////////////////////////////////////////////////////////////////////////////
+// Wood material.
+vec3 matWood(vec3 p) {
+	float n1 = fbmDistorted(p * vec3(7.8, 1.17, 1.17));
+	n1 = mix(n1, 1., .2);
+	float n2 = mix(musgraveFbm(vec3(n1 * 4.6), 8., 0., 2.5), n1, .85),
+	      dirt = 1. - musgraveFbm(waveFbmX(p * vec3(.01, .15, .15)), 15., .26, 2.4) * .4;
+	float grain = 1. - smoothstep(.2, 1., musgraveFbm(p * vec3(500, 6, 1), 2., 2., 2.5)) * .2;
+	n2 *= dirt * grain;
+    
+    // The three vec3 values are the RGB wood colors - Tweak to suit.
+	return mix(mix(vec3(.03, .012, .003), vec3(.25, .11, .04), remap01(n2, .19, .56)), vec3(.52, .32, .19), remap01(n2, .56, 1.));
+}
+
 
 void main() {
     //vec3 closestPoint = (Model * tree[treeIdx].pos).xyz;
@@ -218,13 +266,25 @@ void main() {
             discard;
             return;
         }
+        /*if (t >= 1.0) {
+            p0 = p3;
+            p3 = (Model * tree[tree[treeIdx].children[0]].pos).xyz;
+            p2 = p3 - d * distFactor * dir;// + curve * randomVec3(p3);
+            p1 = p0 + d * distFactor * parentDir;
+        }
+        else if (t < 0.0) {
+            p3 = p0;
+            p0 = (Model * tree[tree[treeIdx].parent].pos).xyz;
+            p2 = p3 - d * distFactor * dir;// + curve * randomVec3(p3);
+            p1 = p0 + d * distFactor * parentDir;
+        }*/
     }
     if (dist > epsilon) {
         discard;
         return;
     }
 
-    vec3 color = vec3(0.5, 0.25, 0.0);
+    vec3 color = pow(matWood(rayPos), vec3(.4545));
     vec3 lightDir = vec3(1.0, 0.0, 0.0);
     float brightness = clamp(dot(lightDir, normalize(rayPos - splinePoint)), 0.1, 1.0);
     //float brightness = 1.0;
